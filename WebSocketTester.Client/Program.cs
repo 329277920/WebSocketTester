0dcf02f80848245b828c2839e6711062e5495245
cnf <nanfei_130@163.com>
2018-04-18 17:44:30 +0800

    添加项目文件。

---
+++ WebSocketTester.Client/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace WebSocketTester.Client
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BuildWebHost(args).Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .UseUrls("http://192.168.10.75:5010")
+                .Build();
+    }
+}
---
+++ WebSocketTester.Library/Class1.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace WebSocketTester.Library
+{
+    public class Class1
+    {
+
+    }
+}
